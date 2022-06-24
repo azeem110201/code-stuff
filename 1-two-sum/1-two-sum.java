3eb758152fb719778349f67f98a3a4a8b28c1ee7
Mohd Abdul Azeem <55446681+azeem110201@users.noreply.github.com>
2022-06-24 09:36:09 +0530

    Time: 124 ms (8.55%), Space: 45.1 MB (58.32%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,15 +1,25 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
+//         HashMap<Integer, Integer> hashmap = new HashMap<>();
         
-        for(int i = 0; i < nums.length; i++){
-            hashmap.put(nums[i], i);
-        }
+//         for(int i = 0; i < nums.length; i++){
+//             hashmap.put(nums[i], i);
+//         }
         
-        for(int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-            if(hashmap.containsKey(complement) && hashmap.get(complement) != i){
-                return new int[]{i, hashmap.get(complement)};
+//         for(int i = 0; i < nums.length; i++){
+//             int complement = target - nums[i];
+//             if(hashmap.containsKey(complement) && hashmap.get(complement) != i){
+//                 return new int[]{i, hashmap.get(complement)};
+//             }
+//         }
+        
+//         return new int[]{-1, -1};
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
             }
         }
         
